bf8ee2d0713413df35c7fd466ca09d82e8ac42d0
Saidu Kamara <mhsaidu@gmail.com>
2025-03-31 08:35:49 -0400

    created a app route for auth/sign-in

--- app.py
+++ app.py
@@ -46,25 +46,47 @@ def verify_token():
        return jsonify({"error": str(error)})
     
 
-    @app.route('/auth/sign-up', methods=['POST'])
-    def signup():
-        try:
-            new_user_data = request.get_json()
-            connection = get_db_connection()
-            cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-            cursor.execute("SELECT * FROM users Where username = %s or email = %s;", (new_user_data["username"], new_user_data["email"],))
-            existing_user = cursor.fetchone()
-            if existing_user:
-                curosr.close()
-                return jsonify({"error": "Username already taken"}), 400
-            hashed_password = bcrypt.hashpw(bytes(new_user_data["password"], 'utf-8'), bcrypt.gensalt())
-            cursor.excute("INSERT INTO users (username,email,password) VALUES(%s, %s, %s) RETURNING id,username", (new_user_data["username"], new_user_data["email"],hashed_password.decode('utf-8')))
-            created_user = cursor.fetchone()
-            connection.commit()
+@app.route('/auth/sign-up', methods=['POST'])
+def signup():
+    try:
+        new_user_data = request.get_json()
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users Where username = %s or email = %s;", (new_user_data["username"], new_user_data["email"],))
+        existing_user = cursor.fetchone()
+        if existing_user:
             cursor.close()
-            connection.close()
-            payload = {"username": created_user["username"], "id": created_user["id"]}
-            token = jwt.encode({ "payload": payload }, os.getenv('JWT_SECRET'))
-            return jsonify({"token": token, "user": created_user}), 201
-        except Exception as err:
+            return jsonify({"error": "Username already taken"}), 400
+        hashed_password = bcrypt.hashpw(bytes(new_user_data["password"], 'utf-8'), bcrypt.gensalt())
+        cursor.excute("INSERT INTO users (username,email,password) VALUES(%s, %s, %s) RETURNING id,username", (new_user_data["username"], new_user_data["email"],hashed_password.decode('utf-8')))
+        created_user = cursor.fetchone()
+        connection.commit()
+        cursor.close()
+        connection.close()
+        payload = {"username": created_user["username"], "id": created_user["id"]}
+        token = jwt.encode({ "payload": payload }, os.getenv('JWT_SECRET'))
+        return jsonify({"token": token, "user": created_user}), 201
+    except Exception as err:
             return jsonify({"error":  str(err)}), 401
+        
+@app.route('/auth/sign-in', methods=["POST"])
+def sign_in():
+    try:
+        sign_in_form_data = request.get_json()
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users WHERE username = %s;", (sign_in_form_data["username"],))
+        existing_user = cursor.fetchone()
+        if existing_user is None:
+            return jsonify({"err": "Wrong Username/Password"}), 401
+        password_is_valid = bcrypt.checkpw(bytes(
+            sign_in_form_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
+        if not password_is_valid:
+            return jsonify({"error": "Invalid credentials."}), 401
+        payload = {"username": existing_user["username"], "id": existing_user["id"]}
+        token = jwt.encode({"payload": payload}, os.getenv('JWT_SECRET'))
+        return jsonify({"token": token}), 200
+    except Exception as err:
+        return jsonify({"err": "Wrong Username/Password."}), 500
+    finally: 
+            connection.close()   
